e31c38386ef30ec642eb195aeafd0eb6c9209905
Benjamin Nour <ben.nour@live.com.au>
2023-12-08 18:35:34 +1100

    updated code of day 3 puzzle

--- day_3.py
+++ day_3.py
@@ -3,6 +3,8 @@ import re
 with open("./data/day_3_puzzle.txt") as f:
     puzzle = f.readlines()
 
+LINE_LENGTH = 140  # Important.
+
 # Part 1.
 
 # Find position of numbers.
@@ -17,7 +19,7 @@ for line_number, line in enumerate(puzzle):
             end = match.end()
             end = end - 1
             middle = end - 1
-            multiplier = line_number * 140
+            multiplier = line_number * LINE_LENGTH
             start = start + multiplier
             middle = middle + multiplier
             end = end + multiplier
@@ -32,7 +34,7 @@ for line_number, line in enumerate(puzzle):
     matches = re.finditer("\*|\#|\+|\$|\&|\/|%|@|=|\-", line)
     for match in matches:
         try:
-            multiplier = line_number * 140
+            multiplier = line_number * LINE_LENGTH
             position = match.start()
             position = position + multiplier
             symbols.append(position)
@@ -46,8 +48,8 @@ for item in numbers:
         for position in positions:
             number_left = position - 1
             number_right = position + 1
-            number_behind = position - 140
-            number_ahead = position + 140
+            number_behind = position - LINE_LENGTH
+            number_ahead = position + LINE_LENGTH
             number_behind_left = number_behind - 1
             number_behind_right = number_behind + 1
             number_ahead_left = number_ahead - 1
