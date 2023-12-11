238df2258347d6e71e4f7d1bda1b723ea4b2b3e1
Benjamin Nour <ben.nour@live.com.au>
2023-12-11 21:23:01 +1100

    Solved day 4 part 2

--- day_4_part_2.py
+++ day_4_part_2.py
@@ -1,51 +1,43 @@
 import re
+import collections
 
-"""
-# Parsing puzzle.
-games = {}
-my_numbers = []
-winning_numbers = []
-with open("./data/day_4_example.txt") as f:
+cards = {}
+with open("./data/day_4_puzzle.txt") as f:
     puzzle = f.readlines()
+    points = []
     for line in puzzle:
-        game_number = re.search("(?<=Card).*(?=:)", line)
-        line_my_numbers = re.search("(?<=\|).*", line)
-        line_my_numbers = line_my_numbers.group().split()
-        my_numbers.append(line_my_numbers)
+        card_number = re.search("(?<=Card).*(?=:)", line)
         line_winning_numbers = re.search("(?<=:).*(?=\|)", line)
-        line_winning_numbers = line_winning_numbers.group().split()
-        winning_numbers.append(line_winning_numbers)
-        games[game_number.group()] = {
-            "my_numbers": my_numbers,
-            "winning_numbers": winning_numbers,
-        }
-"""
-
-# Parsing puzzle.
-my_numbers = []
-winning_numbers = []
-with open("./data/day_4_example.txt") as f:
-    puzzle = f.readlines()
-    for line in puzzle:
+        line_winning_numbers = set(line_winning_numbers.group().split())
         line_my_numbers = re.search("(?<=\|).*", line)
-        line_my_numbers = line_my_numbers.group().split()
-        my_numbers.append(line_my_numbers)
-        line_winning_numbers = re.search("(?<=:).*(?=\|)", line)
-        line_winning_numbers = line_winning_numbers.group().split()
-        winning_numbers.append(line_winning_numbers)
+        line_my_numbers = set(line_my_numbers.group().split())
+        winning_numbers = line_winning_numbers.intersection(line_my_numbers)
+        if winning_numbers:
+            for index, number in enumerate(winning_numbers):
+                if index == 0:
+                    point = 1
+                else:
+                    point = point * 2
+            points.append(point)
+        card_number = card_number.group()
+        cards[card_number.strip()] = len(winning_numbers)
 
-# Identifying winning numbers.
-matches = {}
-for game_number in range(0, 6):
-    matched_numbers = []
-    for my_number in my_numbers[game_number]:
-        if my_number in winning_numbers[game_number]:
-            matched_numbers.append(int(my_number))
-    actual_game_number = game_number + 1
-    copies = len(matched_numbers)
-    matches[actual_game_number] = {
-        "copies": copies
-        # ,"original_numbers": matched_numbers,
-    }
+scratch_cards = []
 
-print(matches)
+
+def get_cards(card):
+    card = int(card)
+    copies = cards.get(str(card))
+    if copies != 0:
+        range_end = copies + 1
+        for item in range(1, range_end):
+            new_card = card + item
+            scratch_cards.append(new_card)
+            get_cards(new_card)
+
+
+for card in cards.keys():
+    get_cards(int(card))
+    scratch_cards.append(int(card))
+
+print(f"Part 1 answer: {len(scratch_cards)}")
