08cc098a7c730341f9dba125e9a7ea59d750ff91
Benjamin Nour <ben.nour@live.com.au>
2023-12-11 14:57:19 +1100

    used set intersection to merge lists rather than list comprehension

--- day_4_part_1.py
+++ day_4_part_1.py
@@ -8,12 +8,10 @@ with open("./data/day_4_puzzle.txt") as f:
     points = []
     for line in puzzle:
         line_winning_numbers = re.search("(?<=:).*(?=\|)", line)
-        line_winning_numbers = line_winning_numbers.group().split()
+        line_winning_numbers = set(line_winning_numbers.group().split())
         line_my_numbers = re.search("(?<=\|).*", line)
-        line_my_numbers = line_my_numbers.group().split()
-        winning_numbers = [
-            number for number in line_winning_numbers if number in line_my_numbers
-        ]
+        line_my_numbers = set(line_my_numbers.group().split())
+        winning_numbers = line_winning_numbers.intersection(line_my_numbers)
         if winning_numbers:
             for index, number in enumerate(winning_numbers):
                 if index == 0:
