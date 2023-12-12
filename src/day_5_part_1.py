0b410823beb78dd197ff1e434832d806579d517b
Benjamin Nour <ben.nour@live.com.au>
2023-12-12 23:01:46 +1100

    working on day 5 solution

--- src/day_5_part_1.py
+++ src/day_5_part_1.py
@@ -1,7 +1,5 @@
 import re
 
-# import pandas as pd
-
 
 def map_sources(map_row):
     mapped = {}
@@ -18,58 +16,57 @@ def map_sources(map_row):
     return mapped
 
 
-with open("./data/day_5_example.txt", "r") as f:
-    lines = f.readlines()
-    maps = []
-    map_row_dictionaries = []
-    total_lines = len(lines)
-    # seed-to-soil, soil-to-fertilizer, etc.
-    for index, line in enumerate(lines):
-        if len(line) != 1:
-            line = line.strip()
-            # Seeds.
-            try:
-                seeds = re.search("(?<=seeds:).*", line).group().split()
-                continue
-            except AttributeError:
-                pass
-            # Check to see if new map.
-            if index + 1 == total_lines:
-                maps.append(map_row_dictionaries)
-            if re.search("^[a-z].*", line) is not None:
-                maps.append(map_row_dictionaries)
-                map_row_dictionaries = []
-            else:
-                try:
-                    line = line.strip()
-                    numbers = re.search("[0-9].*", line).group()
-                    numbers = str(numbers).split()
-                    map_row_dictionaries.append(map_sources(numbers))
-
-                except:
-                    pass
-
-    final_seeds = {}
-    for item in seeds:
-        final_seeds[int(item)] = []
-
-    maps = filter(None, maps)
-    for almanc_map in maps:
+def join_map_rows(almanc_map, seeds):
+    if almanc_map is not None:
+        # Final seeds dictionary.
+        final_seeds = {}
+        for item in seeds:
+            final_seeds[int(item)] = []
         # Joining each map's rows into a final dictionary.
         final_dictionary = {}
         for dictionary in almanc_map:
             final_dictionary = final_dictionary | dictionary
-        # print(final_dictionary)
         # Finding category map location for each seed.
-        # print(final_dictionary)
         for seed in seeds:
-            # print(seed)
             seed = int(seed)
             category = final_dictionary.get(seed)
-            # print(category)
             if category is not None:
-                final_seeds[seed].append(category)
+                final_seeds[seed] = category
             else:
-                final_seeds[seed].append(seed)
+                final_seeds[seed] = seed
+        final_seeds = [value for value in final_seeds.values()]
+        return final_seeds
+    return seeds
 
-    print(final_seeds)
+
+with open("./data/day_5_puzzle.txt", "r") as f:
+    lines = f.readlines()
+    map_row_dictionaries = []
+    total_lines = len(lines)
+    for index, line in enumerate(lines):
+        print(index)
+        line = line.strip()
+        # Seeds.
+        if re.search("(?<=seeds:).*", line):
+            seeds = re.search("(?<=seeds:).*", line).group().split()
+        else:
+            if index + 1 == total_lines:
+                numbers = re.search("[0-9].*", line).group()
+                numbers = str(numbers).split()
+                map_row_dictionaries.append(map_sources(numbers))
+                seeds = join_map_rows(map_row_dictionaries, seeds)
+                print(seeds)  # Part 1 answer.
+            if re.search("^[a-z].*", line) is not None:
+                # print(index, line)
+                seeds = join_map_rows(map_row_dictionaries, seeds)
+                map_row_dictionaries = []
+                # print(seeds)
+            else:
+                try:
+                    numbers = re.search("[0-9].*", line).group()
+                    numbers = str(numbers).split()
+                    map_row_dictionaries.append(map_sources(numbers))
+                except:
+                    pass
+
+    # maps = filter(None, maps)
